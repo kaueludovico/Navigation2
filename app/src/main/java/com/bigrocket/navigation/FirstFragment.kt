e2aae0fc20b0d482663d2ab81984deda4ad76b09
kaueludovico <kaueludovico@outlook.com>
2022-12-12 13:52:08 -0300

    Passagem de dados funcionando

--- app/src/main/java/com/bigrocket/navigation/FirstFragment.kt
+++ app/src/main/java/com/bigrocket/navigation/FirstFragment.kt
@@ -3,6 +3,7 @@ package com.bigrocket.navigation
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
@@ -11,8 +12,14 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val data = view.findViewById<EditText>(R.id.data)
+
+
         view.findViewById<Button>(R.id.first).setOnClickListener{
-            findNavController().navigate(R.id.actionFirstToSecondFragment)
+            val action = FirstFragmentDirections.actionFirstToSecondFragment(
+                "${data.text}"
+            )
+            findNavController().navigate(action)
         }
     }
 }
--- app/src/main/java/com/bigrocket/navigation/SecondFragment.kt
+++ app/src/main/java/com/bigrocket/navigation/SecondFragment.kt
@@ -3,17 +3,22 @@ package com.bigrocket.navigation
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
 
+    private val args : SecondFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.result).text = "${args.dado}"
 
         view.findViewById<Button>(R.id.second).setOnClickListener {
             findNavController().navigate(R.id.actionSecondToFirstFragment)
         }
     }
-
 }
